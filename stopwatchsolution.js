373f1bd3aa4ae1daba14e4c3b57dcb43ff561b59
Annabelle <51344723+annabelledelacruz1825@users.noreply.github.com>
2020-05-05 16:16:48 -0400

    Update stopwatchsolution.js

--- stopwatchsolution.js
+++ stopwatchsolution.js
@@ -1,207 +1,180 @@
-// GLOBAL VARIABLES (accessible by all functions)
-// ==================================================================================================
+// STOPWATCH ACTIVITY (SOLUTION)
+// =============================
 
-// Array of Word Options (all lowercase).
-var wordsList = ["jerome", "neena", "darion", "lou", "greg", "jordan",
-  "jasmine", "stephen", "jacob", "adam", "rui", "luis"];
-
-// Computer selected solution will be held here.
-var chosenWord = "";
-
-// This will break the solution into individual letters to be stored in array.
-var lettersInChosenWord = [];
-
-// This will be the number of blanks we show based on the solution.
-var numBlanks = 0;
-
-// Holds a mix of blank and solved letters (ex: 'n, _ _, n, _').
-var blanksAndSuccesses = [];
-
-// Holds all of the wrong guesses.
-var wrongGuesses = [];
-
-// Holds the letters guessed
-var letterGuessed = "";
-
-// Game counters
-var winCounter = 0;
-var lossCounter = 0;
-var numGuesses = 9;
-
-// FUNCTIONS (These are bits of code that we will call upon to run when needed).
-// ==================================================================================================
-
-// startGame()
-// It's how we we will start and restart the game.
-// (Note: It's not being run here. Function declarations like this are made for future use.)
-function startGame() {
-
-  // Reset the guesses back to 0.
-  numGuesses = 9;
-
-  // Solution chosen randomly from wordList.
-  chosenWord = wordsList[Math.floor(Math.random() * wordsList.length)];
-
-  // The word is broken into individual letters.
-  lettersInChosenWord = chosenWord.split("");
-
-  // We count the number of letters in the word.
-  numBlanks = lettersInChosenWord.length;
-
-  // We print the solution in console (for testing).
-  console.log(chosenWord);
-
-  // CRITICAL LINE
-  // Here we *reset* the guess and success array at each round.
-  blanksAndSuccesses = [];
-
-  // CRITICAL LINE
-  // Here we *reset* the wrong guesses from the previous round.
-  wrongGuesses = [];
-
-  // Fill up the blanksAndSuccesses list with appropriate number of blanks.
-  // This is based on number of letters in solution.
-  for (var i = 0; i < numBlanks; i++) {
-    blanksAndSuccesses.push("_");
-  }
-
-  // Print the initial blanks in console.
-  console.log(blanksAndSuccesses);
-
-  // Reprints the guessesLeft to 9.
-  document.getElementById("guesses-left").innerHTML = numGuesses;
-
-  // Prints the blanks at the beginning of each round in the HTML.
-  document.getElementById("word-blanks").innerHTML = blanksAndSuccesses.join(" ");
-
-  // Clears the wrong guesses from the previous round.
-  document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
-}
-
-// checkLetters() function
-// It's where we will do all of the comparisons for matches.
-// Again, it's not being called here. It's just being made for future use.
-function checkLetters(letter) {
-
-  // This boolean will be toggled based on whether or not
-  // a user letter is found anywhere in the word.
-  var letterInWord = false;
-
-  // Check if a letter exists inside the array at all.
-  for (var i = 0; i < numBlanks; i++) {
-
-    if (chosenWord[i] === letter) {
-
-      // If the letter exists then toggle this boolean to true.
-      // This will be used in the next step.
-      letterInWord = true;
-    }
-  }
-
-  // If the letter exists somewhere in the word,
-  // then figure out exactly where (which indices).
-  if (letterInWord) {
-
-    // Loop through the word
-    for (var j = 0; j < numBlanks; j++) {
-
-      // Populate the blanksAndSuccesses with every instance of the letter.
-      if (chosenWord[j] === letter) {
-
-        // Here we set specific blank spaces to equal the correct letter
-        // when there is a match.
-        blanksAndSuccesses[j] = letter;
-      }
-    }
-
-    // Log the current blanks and successes for testing.
-    console.log(blanksAndSuccesses);
-  }
-
-  // If the letter doesn't exist at all...
-  else {
-
-    // Then we add the letter to the list of wrong letters.
-    wrongGuesses.push(letter);
-
-    // We also subtract one of the guesses.
-    numGuesses--;
-
-  }
-
-}
-
-// roundComplete() function
-// Here we will have all of the code that needs to be run after each guess is made.
-function roundComplete() {
-
-  // First, log an initial status update in the console
-  // telling us how many wins, losses, and guesses are left.
-  console.log("WinCount: " + winCounter + " | LossCount: " + lossCounter + " | NumGuesses: " + numGuesses);
-
-  // HTML UPDATES
-  // ============
-
-  // Update the HTML to reflect the new number of guesses.
-  document.getElementById("guesses-left").innerHTML = numGuesses;
-
-  // This will print the array of guesses and blanks onto the page.
-  document.getElementById("word-blanks").innerHTML = blanksAndSuccesses.join(" ");
-
-  // This will print the wrong guesses onto the page.
-  document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
-
-  // If our Word Guess string equals the solution.
-  // (meaning that we guessed all the letters to match the solution)...
-  if (lettersInChosenWord.toString() === blanksAndSuccesses.toString()) {
-
-    // Add to the win counter
-    winCounter++;
-
-    // Give the user an alert
-    alert("You win!");
-
-    // Update the win counter in the HTML
-    document.getElementById("win-counter").innerHTML = winCounter;
-
-    // Restart the game
-    startGame();
-  }
-
-  // If we've run out of guesses
-  else if (numGuesses === 0) {
-
-    // Add to the loss counter
-    lossCounter++;
-
-    // Give the user an alert
-    alert("You lose");
-
-    // Update the loss counter in the HTML
-    document.getElementById("loss-counter").innerHTML = lossCounter;
-
-    // Restart the game
-    startGame();
-
-  }
-
-}
-
-// MAIN PROCESS (THIS IS THE CODE THAT CONTROLS WHAT IS ACTUALLY RUN)
-// ==================================================================
-
-// Starts the Game by running the startGame() function
-startGame();
-
-// Then initiates the function for capturing key clicks.
-document.onkeyup = function(event) {
-
-  // Converts all key clicks to lowercase letters.
-  letterGuessed = String.fromCharCode(event.which).toLowerCase();
-
-  // Runs the code to check for correct guesses.
-  checkLetters(letterGuessed);
-
-  // Runs the code that ends each round.
-  roundComplete();
+// This code will run as soon as the page loads
+window.onload = function() {
+  $("#lap").on("click", recordLap);
+  $("#stop").on("click", stop);
+  $("#reset").on("click", reset);
+  $("#start").on("click", start);
 };
+
+//  Variable that will hold our setInterval that runs the stopwatch
+var intervalId;
+
+// prevents the clock from being sped up unnecessarily
+var clockRunning = false;
+var time = 0;
+var lap = 1;
+
+function reset() {
+
+  time = 0;
+  lap = 1;
+
+  // DONE: Change the "display" div to "00:00."
+  $("#display").text("00:00");
+
+  // DONE: Empty the "laps" div.
+  $("#laps").text("");
+}
+function start() {
+
+  // DONE: Use setInterval to start the count here and set the clock to running.
+  if (!clockRunning) {
+    intervalId = setInterval(count, 1000);
+    clockRunning = true;
+  }
+}
+function stop() {
+
+  // DONE: Use clearInterval to stop the count here and set the clock to not be running.
+  clearInterval(intervalId);
+  clockRunning = false;
+}
+function recordLap() {
+
+  // DONE: Get the current time, pass that into the timeConverter function,
+  //       and save the result in a variable.
+  var converted = timeConverter(time);
+
+  // DONE: Add the current lap and time to the "laps" div.
+  $("#laps").append("<p>Lap " + lap + " : " + converted + "</p>");
+
+  // DONE: Increment lap by 1. Remember, we can't use "this" here.
+  lap++;
+}
+function count() {
+
+  // DONE: increment time by 1, remember we cant use "this" here.
+  time++;
+
+  // DONE: Get the current time, pass that into the timeConverter function,
+  //       and save the result in a variable.
+  var converted = timeConverter(time);
+  console.log(converted);
+
+  // DONE: Use the variable we just created to show the converted time in the "display" div.
+  $("#display").text(converted);
+}
+function timeConverter(t) {
+
+  var minutes = Math.floor(t / 60);
+  var seconds = t - (minutes * 60);
+
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+
+  if (minutes === 0) {
+    minutes = "00";
+  }
+  else if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  return minutes + ":" + seconds;
+}
+
+
+// Solution if you would like to put it in an object
+
+// window.onload = function() {
+//   $("#lap").on("click", stopwatch.recordLap);
+//   $("#stop").on("click", stopwatch.stop);
+//   $("#reset").on("click", stopwatch.reset);
+//   $("#start").on("click", stopwatch.start);
+// };
+
+// //  Variable that will hold our setInterval that runs the stopwatch
+// var intervalId;
+
+// // prevents the clock from being sped up unnecessarily
+// var clockRunning = false;
+
+// // Our stopwatch object
+// var stopwatch = {
+
+//   time: 0,
+//   lap: 1,
+
+//   reset: function() {
+
+//     stopwatch.time = 0;
+//     stopwatch.lap = 1;
+
+//     // DONE: Change the "display" div to "00:00."
+//     $("#display").text("00:00");
+
+//     // DONE: Empty the "laps" div.
+//     $("#laps").text("");
+//   },
+//   start: function() {
+
+//     // DONE: Use setInterval to start the count here and set the clock to running.
+//     if (!clockRunning) {
+//       intervalId = setInterval(stopwatch.count, 1000);
+//       clockRunning = true;
+//     }
+//   },
+//   stop: function() {
+
+//     // DONE: Use clearInterval to stop the count here and set the clock to not be running.
+//     clearInterval(intervalId);
+//     clockRunning = false;
+//   },
+//   recordLap: function() {
+
+//     // DONE: Get the current time, pass that into the stopwatch.timeConverter function,
+//     //       and save the result in a variable.
+//     var converted = stopwatch.timeConverter(stopwatch.time);
+
+//     // DONE: Add the current lap and time to the "laps" div.
+//     $("#laps").append("<p>Lap " + stopwatch.lap + " : " + converted + "</p>");
+
+//     // DONE: Increment lap by 1. Remember, we can't use "this" here.
+//     stopwatch.lap++;
+//   },
+//   count: function() {
+
+//     // DONE: increment time by 1, remember we cant use "this" here.
+//     stopwatch.time++;
+
+//     // DONE: Get the current time, pass that into the stopwatch.timeConverter function,
+//     //       and save the result in a variable.
+//     var converted = stopwatch.timeConverter(stopwatch.time);
+//     console.log(converted);
+
+//     // DONE: Use the variable we just created to show the converted time in the "display" div.
+//     $("#display").text(converted);
+//   },
+//   timeConverter: function(t) {
+
+//     var minutes = Math.floor(t / 60);
+//     var seconds = t - (minutes * 60);
+
+//     if (seconds < 10) {
+//       seconds = "0" + seconds;
+//     }
+
+//     if (minutes === 0) {
+//       minutes = "00";
+//     }
+//     else if (minutes < 10) {
+//       minutes = "0" + minutes;
+//     }
+
+//     return minutes + ":" + seconds;
+//   }
+// };
+
